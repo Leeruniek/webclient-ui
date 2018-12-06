2b5b8ab048b726b9ab099a8262c83ce83faa2aa6
whois42 <gunchenko@htmlacademy.ru>
2018-12-06 14:13:21 +0100

    Checkbox component

---
+++ src/checkbox.page/checkbox/checkbox.jsx
@@ -0,0 +1,39 @@
+// @flow
+
+const debug = require("debug")("WebclientUI:LUCheckbox")
+
+import React from "react"
+import cx from "classnames"
+import { isEmpty } from "@leeruniek/functies"
+
+import css from "./css/checkbox.css"
+
+
+type LUCheckboxPropsType = {|
+  className: string,
+  customStyle: string,
+  label: string,
+  isDisabled: boolean,
+  isChecked: boolean,
+  onChange: Function
+|}
+
+const LUCheckbox = React.memo((props: LUCheckboxPropsType): React.Node  =>  (
+    <label className={cx(css.field, {
+      [props.className]: !isEmpty(props.className),
+      [css[props.customStyle]]: !isEmpty(props.customStyle),
+    })}>
+      <input
+        type="checkbox"
+        disabled={props.isDisabled}
+        checked={props.isChecked}
+        className={css.input}
+        onChange={props.onChange}
+      />
+      <div className={cx(css.check, { [css.checked]: props.isChecked })}/>
+      {props.label ? <span className={css.text}>{props.label}</span> : null}
+    </label>
+  )
+)
+
+export { LUCheckbox }
--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,13 @@ import React from "react"
 import { render } from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
-import { LUButtonPage } from "./button.page/button.page"
+import { LUCheckboxPage } from "./checkbox.page/checkbox.page"
 import "./index.css"
 
 render(
   <BrowserRouter>
     <Switch>
-      <Route exact={true} path="/" component={LUButtonPage} />
+      <Route exact={true} path="/" component={LUCheckboxPage} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
