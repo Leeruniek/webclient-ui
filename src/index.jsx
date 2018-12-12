74649bab17635ca939c261230ec7acba867dd88b
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2018-12-12 13:42:27 +0100

    Add all pages to router

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,22 @@ import React from "react"
 import { render } from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
-import { LUCheckboxGroupPage } from "./checkbox-group/checkbox-group.page"
+import { LUButtonPage } from "./button/button.page"
+import { LUCheckboxPage } from "./checkbox/checkbox.page"
+import { LUCheckboxGroupPage } from "./checkbox-group/checkbox-group"
+
 import "./index.css"
 
 render(
   <BrowserRouter>
     <Switch>
-      <Route exact={true} path="/" component={LUCheckboxGroupPage} />
+      <Route exact={true} path="/button" component={LUButtonPage} />
+      <Route exact={true} path="/checkbox" component={LUCheckboxPage} />
+      <Route
+        exact={true}
+        path="/checkbox-group"
+        component={LUCheckboxGroupPage}
+      />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
