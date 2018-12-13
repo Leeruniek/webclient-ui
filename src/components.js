7b0cd85321b5d07f55eca6629b41f814b0627c9c
whois42 <gunchenko@htmlacademy.ru>
2018-12-13 09:21:44 +0100

    Export LUCheckboxGroupHeader

--- src/components.js
+++ src/components.js
@@ -1,3 +1,4 @@
 export { LUButton } from "./button/button"
 export { LUCheckbox } from "./checkbox/checkbox"
 export { LUCheckboxGroup } from "./checkbox-group/checkbox-group"
+export { LUCheckboxGroupHeader } from "./checkbox-group/checkbox-group__header"
