72d6b67b9cb1da6ee02849e7c1c1ea18e55492fa
whois42 <gunchenko@htmlacademy.ru>
2019-04-11 16:55:38 +0200

    LULegend

---
+++ src/legend/legend.jsx
@@ -0,0 +1,47 @@
+// @flow
+
+const debug = require("debug")("Leeruniek:LULegend")
+
+import React from "react"
+import cx from "classnames"
+import { is, isEmpty } from "@leeruniek/functies"
+
+import css from "./legend.module.css"
+
+type LegendItemType = {|
+  className: string,
+  label: string,
+|}
+
+type LULegendType = {|
+  className?: string,
+  type?: "block" | "inline",
+  title?: string,
+  items: LegendItemType[],
+|}
+
+export const LULegend = React.memo<LULegendType>(
+  ({ className, type = "block", title, items }: LULegendType) => {
+    return (
+      <div
+        className={cx(css.legend, css[`legend--${type}`], {
+          [className || ""]: !isEmpty(className),
+        })}>
+        {is(title) ? <div className="legend__title">{title}</div> : null}
+        {items.map((item: LegendItemType, idx: number) => (
+          <div key={`legend-item-${idx}`} className={css.legend__item}>
+            <i
+              className={cx(
+                "fa",
+                "fa-square",
+                css.legend__item__color,
+                item.className
+              )}
+            />
+            <div className={css.legend__item__label}>{item.label}</div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+)
