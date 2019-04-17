fbfc0d40225b113c7d1c8ca3c8803a95fa7d38f9
whois42 <gunchenko@htmlacademy.ru>
2019-04-17 11:26:29 +0200

    Migration to webpack + Add storybook

--- src/index.jsx
+++ src/index.jsx
@@ -8,22 +8,40 @@ import { LUCheckboxGroupPage } from "./checkbox-group/checkbox-group.page"
 import { LUTagPage } from "./tag/tag.page"
 
 import "./index.css"
-
-render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact={true} path="/button" component={LUButtonPage} />
-      <Route exact={true} path="/checkbox" component={LUCheckboxPage} />
-      <Route
-        exact={true}
-        path="/checkbox-group"
-        component={LUCheckboxGroupPage}
-      />
-      <Route exact={true} path="/tag" component={LUTagPage} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-)
+class App extends React.Component {
+  render = () => {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/button" component={LUButtonPage} />
+          <Route exact={true} path="/checkbox" component={LUCheckboxPage} />
+          <Route
+            exact={true}
+            path="/checkbox-group"
+            component={LUCheckboxGroupPage}
+          />
+          <Route exact={true} path="/tag" component={LUTagPage} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+render(<App />, document.getElementById("root"))
+// render(
+//   <BrowserRouter>
+//     <Switch>
+//       <Route exact={true} path="/button" component={LUButtonPage} />
+//       <Route exact={true} path="/checkbox" component={LUCheckboxPage} />
+//       <Route
+//         exact={true}
+//         path="/checkbox-group"
+//         component={LUCheckboxGroupPage}
+//       />
+//       <Route exact={true} path="/tag" component={LUTagPage} />
+//     </Switch>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// )
 
 if (module.hot) {
   module.hot.accept()
