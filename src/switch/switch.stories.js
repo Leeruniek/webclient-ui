d02fbe9957fbabba35867bdba51edad8d81b5e99
whois42 <gunchenko@htmlacademy.ru>
2019-04-25 17:09:23 +0200

    Change color to color-mod

---
+++ src/switch/switch.stories.js
@@ -0,0 +1,5 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import { LUSwitch } from "./switch"
+
+storiesOf("LUSwitch", module).add("LUSwitch", () => <LUSwitch label="LUSwitch" />)
