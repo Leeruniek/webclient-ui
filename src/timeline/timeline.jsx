e12f1c346ac8c12683ef4bb355c7c79c2bbe4624
whois42 <jgunchenko@gmail.com>
2019-07-09 09:34:05 +0200

    Add tooltips and expanding

--- src/timeline/timeline.jsx
+++ src/timeline/timeline.jsx
@@ -3,20 +3,22 @@
 import * as React from "react"
 import cx from "classnames"
 import { is, pipe, sortBy, map } from "@leeruniek/functies"
+import ReactTooltip from "react-tooltip"
 import { format } from "date-fns"
 
 import css from "./timeline.css"
 
-const nlLocale = require('date-fns/locale/nl')
+const nlLocale = require("date-fns/locale/nl")
 
 const { Fragment } = React
 
 type TimelineItemType = {|
-  key: string,
+  id: number,
   icon: React.Node,
   title: string,
   date: string,
   user: string,
+  tooltipText: string,
   subjectId?: number,
   color?: string,
   className: ?string,
@@ -40,57 +42,83 @@ const LUTimeline = ({
   titleClassName,
   dateClassName,
   renderChild,
-}: Props): React.Node => (
-  <Fragment>
-    {pipe(
-      sortBy("date", sortDirection || "desc"),
-      map(
-        (item): React.Node => (
-          <div
-            key={item.key}
-            className={cx(css.timeline__item, itemClassName, item.className)}>
-            <div className={css.timeline__timeline}>
-              <div className={css["timeline__item-icon-container"]}>
-                <i
+}: Props): React.Node => {
+  const [isTimelineExpanded, setIsTimelineExpanded] = React.useState(false)
+
+  const visibleItems =
+    isTimelineExpanded || items.length <= 3
+      ? sortBy("date", sortDirection || "desc")(items)
+      : sortBy("date", sortDirection || "desc")(items).slice(0, 3)
+
+  return (
+    <Fragment>
+      {pipe(
+        map(
+          (item): React.Node => (
+            <div
+              key={`timeline_item_${item.id}`}
+              className={cx(css.timeline__item, itemClassName, item.className)}>
+              <div className={css.timeline__timeline}>
+                <div
+                  className={css["timeline__item-icon-container"]}
+                  data-tip={true}
+                  data-for={`timeline-tooltip-${item.id}`}>
+                  <i
+                    className={cx(
+                      css["timeline__item-icon"],
+                      "fa",
+                      `fa-${item.icon}`
+                    )}
+                    style={{ color: item.color }}
+                  />
+                </div>
+                <ReactTooltip id={`timeline-tooltip-${item.id}`} effect="solid">
+                  {item.tooltipText}
+                </ReactTooltip>
+              </div>
+              <p className={css["timeline__item-meta-container"]}>
+                <strong
                   className={cx(
-                    css["timeline__item-icon"],
-                    "fa",
-                    `fa-${item.icon}`
-                  )}
-                  style={{ color: item.color }}
-                />
-              </div>
+                    css["timeline__item-title"],
+                    titleClassName,
+                    item.titleClassName
+                  )}>
+                  {item.title}
+                </strong>
+                <br />
+                <small
+                  className={cx(
+                    css["timeline__item-date"],
+                    dateClassName,
+                    item.dateClassName
+                  )}>
+                  {format(item.date, "DD MMMM YYYY HH:mm", {
+                    locale: nlLocale,
+                  })}
+                  {item.user && <Fragment> | {item.user}</Fragment>}
+                </small>
+              </p>
+              {is(renderChild) && (
+                <div className={css["timeline__item-child-container"]}>
+                  {renderChild(item)}
+                </div>
+              )}
             </div>
-            <p className={css["timeline__item-meta-container"]}>
-              <strong
-                className={cx(
-                  css["timeline__item-title"],
-                  titleClassName,
-                  item.titleClassName
-                )}>
-                {item.title}
-              </strong>
-              <br />
-              <small
-                className={cx(
-                  css["timeline__item-date"],
-                  dateClassName,
-                  item.dateClassName
-                )}>
-                {format(item.date, "DD MMMM YYYY HH:mm", {locale: nlLocale})}
-                {item.user && <Fragment> | {item.user}</Fragment>}
-              </small>
-            </p>
-            {is(renderChild) && (
-              <div className={css["timeline__item-child-container"]}>
-                {renderChild(item)}
-              </div>
-            )}
-          </div>
+          )
         )
-      )
-    )(items)}
-  </Fragment>
-)
+      )(visibleItems)}
+      {items.length > 3 ? (
+        <a
+          onClick={event => {
+            event.preventDefault()
+            setIsTimelineExpanded(!isTimelineExpanded)
+          }}
+          className={css["timeline_expand-ctrl"]}>
+          {isTimelineExpanded ? "Toon minder" : "Toon meer"}
+        </a>
+      ) : null}
+    </Fragment>
+  )
+}
 
 export { LUTimeline }
